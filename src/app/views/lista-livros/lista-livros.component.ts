66841e26e2963964a0235b041a0f6632a2362511
AbnerRS <abnersesisenai@gmail.com>
2023-09-19 10:47:31 -0300

    V5 - Otimizando a busca com RXJS.

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,22 +1,62 @@
-import { Subscription } from 'rxjs';
-import { Component, OnDestroy } from '@angular/core';
+import { switchMap, map, tap, filter, debounceTime, throwError, catchError, EMPTY, of } from 'rxjs';
+import { Component } from '@angular/core';
 import { LivroService } from 'src/app/service/livro.service';
-import { Livro } from 'src/app/models/interfaces';
+<<<<<<< HEAD
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+import { FormControl } from '@angular/forms';
+
+const PAUSA = 300;
+=======
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
+>>>>>>> c1d0f77e8c9832c1216d036308c5d09b7083e0eb
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy{
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
-  campoBusca: string = ''
-  subscription: Subscription
-  livro: Livro
+  campoBusca = new FormControl();
+  mensagemErro = ''
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) { }
 
+<<<<<<< HEAD
+  // totalDeLivros$ = this.campoBusca.valueChanges
+  // .pipe(
+  //   debounceTime(PAUSA),
+  //   filter((valorDigitado) => valorDigitado.length >= 3),
+  //   tap(() => console.log('Fluxo inicial')),
+  //   switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+  //   map(resultado => this.livrosResultado = resultado),
+  //   catchError(erro => {
+  //     console.log(erro)
+  //     return of()
+  //   })
+  // )
+
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
+      tap((retornoAPI) => console.log(retornoAPI)),
+      map(resultado => resultado.items ?? []),
+      map((items) => this.livrosResultadoParaLivros(items)),
+      catchError((erro) => {
+        // this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'
+        // return EMPTY
+        console.log(erro)
+        return throwError(() => new Error(this.mensagemErro ='Ops, ocorreu um erro. Recarregue a aplicação!'))
+      })
+    )
+=======
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
       next: (items) => {
@@ -24,29 +64,24 @@ export class ListaLivrosComponent implements OnDestroy{
       },
       error: erro => console.error(erro),
     }
-
     )
   }
+>>>>>>> c1d0f77e8c9832c1216d036308c5d09b7083e0eb
 
-  livrosResultadoParaLivros(items): Livro[] {
-    const livros: Livro[] = []
-
-    items.forEach(item => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
-      })
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
     })
-
-    return livros
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe()
-  }
 }
+
+
+
+<<<<<<< HEAD
+function trhowError(arg0: () => Error) {
+  throw new Error('Function not implemented.');
+}
+
+=======
+>>>>>>> c1d0f77e8c9832c1216d036308c5d09b7083e0eb
